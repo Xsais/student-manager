faf8f6b22587dd9800b24dfc9e78de27dea2654a
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-07-28 01:18:41 -0400

    Created usercontrol for easier use

---
+++ student-manager/ui/functionality/SearchBox.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace student_manager.ui.functionality
+{
+    partial class SearchBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearchBox = new System.Windows.Forms.TextBox();
+            this.picSearch = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picSearch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearchBox
+            // 
+            this.txtSearchBox.BackColor = System.Drawing.SystemColors.Window;
+            this.txtSearchBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearchBox.Location = new System.Drawing.Point(3, 7);
+            this.txtSearchBox.Name = "txtSearchBox";
+            this.txtSearchBox.Size = new System.Drawing.Size(228, 13);
+            this.txtSearchBox.TabIndex = 7;
+            // 
+            // picSearch
+            // 
+            this.picSearch.BackColor = System.Drawing.SystemColors.Window;
+            this.picSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picSearch.Image = global::student_manager.Properties.Resources.search;
+            this.picSearch.Location = new System.Drawing.Point(237, 3);
+            this.picSearch.Name = "picSearch";
+            this.picSearch.Size = new System.Drawing.Size(21, 21);
+            this.picSearch.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSearch.TabIndex = 8;
+            this.picSearch.TabStop = false;
+            this.picSearch.Click += new System.EventHandler(this.Search);
+            // 
+            // SearchBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.picSearch);
+            this.Controls.Add(this.txtSearchBox);
+            this.MaximumSize = new System.Drawing.Size(261, 27);
+            this.MinimumSize = new System.Drawing.Size(261, 27);
+            this.Name = "SearchBox";
+            this.Size = new System.Drawing.Size(261, 25);
+            ((System.ComponentModel.ISupportInitialize)(this.picSearch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSearch;
+        private System.Windows.Forms.TextBox txtSearchBox;
+    }
+}
---
+++ student-manager/ui/functionality/SearchBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student_manager.ui.functionality
+{
+    public partial class SearchBox : UserControl
+    {
+        public EventHandler Searched;
+
+        public override string Text { get => txtSearchBox.Text; set => txtSearchBox.Text = value; }
+
+        public SearchBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Search(object sender, EventArgs e) => Searched?.Invoke(this, e);
+    }
+}
