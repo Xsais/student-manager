14dde9433ea2ea0727ee3e7d8b589a244e4ce5a3
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-07-28 15:33:06 -0400

    Added customizations to the look and feel

--- student-manager/ui/display/ClickableDisplay.cs
+++ student-manager/ui/display/ClickableDisplay.cs
@@ -13,36 +13,46 @@ namespace student_manager.ui.display
     public partial class ClickableDisplay : UserControl
     {
         private bool _isSelected;
-        
+
         public Color HighlightColor { get; set; } = SystemColors.Highlight;
 
         public Color UnderlineColor { get; set; } = Color.Black;
 
+        public int UnderlineHight { get => lblUderLine.Height; set => lblUderLine.Height = value; }
+
+        public Cursor Cursor { get => lblTitle.Cursor; set => lblTitle.Cursor = value; }
+
         public Color SelectionColor { get; set; } = SystemColors.Highlight;
-        
+
         public bool IsSelected
         {
-            get
-            {
-                
-                return _isSelected;
-            }
+            get { return _isSelected; }
             set
             {
-                
                 _isSelected = value;
-                if (!_isSelected)
+                if (UnderlineColor.Equals(SelectionColor))
                 {
 
+                    return;
+                }
+
+                if (!_isSelected)
+                {
                     lblUderLine.BackColor = UnderlineColor;
                     return;
                 }
-                
+
                 lblUderLine.BackColor = SelectionColor;
             }
         }
 
-        public override Font Font { get => lblTitle.Font; set => lblTitle.Font = value; }
+        public ContentAlignment TextAlign { get => lblTitle.TextAlign; set => lblTitle.TextAlign = value; }
+
+        public override Font Font
+        {
+            get => lblTitle.Font;
+            set => lblTitle.Font = value;
+        }
 
         public string Title
         {
@@ -57,7 +67,7 @@ namespace student_manager.ui.display
 
         private void HiglightClickable(object sender, EventArgs e)
         {
-            if (IsSelected)
+            if (IsSelected || HighlightColor.Equals(UnderlineColor))
             {
                 return;
             }
@@ -67,7 +77,7 @@ namespace student_manager.ui.display
 
         private void DeHighlightClickable(object sender, EventArgs e)
         {
-            if (IsSelected)
+            if (IsSelected || HighlightColor.Equals(UnderlineColor))
             {
                 return;
             }
