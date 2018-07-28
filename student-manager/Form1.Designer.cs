d4c9d8a4b974661ec27805801b394afc241c5f53
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-07-28 15:46:53 -0400

    Added pages and changed some UI elements

--- student-manager/Form1.Designer.cs
+++ student-manager/Form1.Designer.cs
@@ -30,17 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pnlStudents = new System.Windows.Forms.Panel();
-            this.searchBox = new student_manager.ui.functionality.SearchBox();
             this.picLink = new System.Windows.Forms.PictureBox();
             this.picEdit = new System.Windows.Forms.PictureBox();
             this.picMinus = new System.Windows.Forms.PictureBox();
             this.picAdd = new System.Windows.Forms.PictureBox();
-            this.edStudents = new student_manager.ui.display.EntityDisplayGroup();
             this.cdProfessors = new student_manager.ui.display.ClickableDisplay();
             this.cdStudents = new student_manager.ui.display.ClickableDisplay();
             this.cdCourses = new student_manager.ui.display.ClickableDisplay();
             this.cdPrograms = new student_manager.ui.display.ClickableDisplay();
             this.cdHome = new student_manager.ui.display.ClickableDisplay();
+            this.iPage = new student_manager.ui.display.Indicator();
+            this.cdPage = new student_manager.ui.display.ClickableDisplay();
+            this.searchBox = new student_manager.ui.functionality.SearchBox();
+            this.edStudents = new student_manager.ui.display.EntityDisplayGroup();
             this.pnlStudents.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picLink)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picEdit)).BeginInit();
@@ -51,6 +53,8 @@
             // pnlStudents
             // 
             this.pnlStudents.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlStudents.Controls.Add(this.iPage);
+            this.pnlStudents.Controls.Add(this.cdPage);
             this.pnlStudents.Controls.Add(this.searchBox);
             this.pnlStudents.Controls.Add(this.picLink);
             this.pnlStudents.Controls.Add(this.picEdit);
@@ -60,20 +64,9 @@
             this.pnlStudents.Dock = System.Windows.Forms.DockStyle.Right;
             this.pnlStudents.Location = new System.Drawing.Point(145, 0);
             this.pnlStudents.Name = "pnlStudents";
-            this.pnlStudents.Size = new System.Drawing.Size(646, 461);
+            this.pnlStudents.Size = new System.Drawing.Size(646, 481);
             this.pnlStudents.TabIndex = 0;
             this.pnlStudents.Text = "grpStudents";
-            // 
-            // searchBox
-            // 
-            this.searchBox.BackColor = System.Drawing.SystemColors.Window;
-            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.searchBox.Location = new System.Drawing.Point(337, 32);
-            this.searchBox.MaximumSize = new System.Drawing.Size(261, 31);
-            this.searchBox.MinimumSize = new System.Drawing.Size(261, 31);
-            this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(261, 31);
-            this.searchBox.TabIndex = 6;
             // 
             // picLink
             // 
@@ -122,12 +115,123 @@
             this.picAdd.TabIndex = 1;
             this.picAdd.TabStop = false;
             // 
+            // cdProfessors
+            // 
+            this.cdProfessors.HighlightColor = System.Drawing.SystemColors.Highlight;
+            this.cdProfessors.IsSelected = false;
+            this.cdProfessors.Location = new System.Drawing.Point(12, 274);
+            this.cdProfessors.Name = "cdProfessors";
+            this.cdProfessors.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.cdProfessors.Size = new System.Drawing.Size(110, 26);
+            this.cdProfessors.TabIndex = 5;
+            this.cdProfessors.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.cdProfessors.Title = "Professors";
+            this.cdProfessors.UnderlineColor = System.Drawing.Color.Black;
+            this.cdProfessors.UnderlineHight = 3;
+            // 
+            // cdStudents
+            // 
+            this.cdStudents.HighlightColor = System.Drawing.SystemColors.Highlight;
+            this.cdStudents.IsSelected = false;
+            this.cdStudents.Location = new System.Drawing.Point(12, 215);
+            this.cdStudents.Name = "cdStudents";
+            this.cdStudents.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.cdStudents.Size = new System.Drawing.Size(110, 26);
+            this.cdStudents.TabIndex = 4;
+            this.cdStudents.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.cdStudents.Title = "Students";
+            this.cdStudents.UnderlineColor = System.Drawing.Color.Black;
+            this.cdStudents.UnderlineHight = 3;
+            // 
+            // cdCourses
+            // 
+            this.cdCourses.HighlightColor = System.Drawing.SystemColors.Highlight;
+            this.cdCourses.IsSelected = false;
+            this.cdCourses.Location = new System.Drawing.Point(12, 152);
+            this.cdCourses.Name = "cdCourses";
+            this.cdCourses.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.cdCourses.Size = new System.Drawing.Size(110, 26);
+            this.cdCourses.TabIndex = 3;
+            this.cdCourses.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.cdCourses.Title = "Courses";
+            this.cdCourses.UnderlineColor = System.Drawing.Color.Black;
+            this.cdCourses.UnderlineHight = 3;
+            // 
+            // cdPrograms
+            // 
+            this.cdPrograms.HighlightColor = System.Drawing.SystemColors.Highlight;
+            this.cdPrograms.IsSelected = false;
+            this.cdPrograms.Location = new System.Drawing.Point(12, 91);
+            this.cdPrograms.Name = "cdPrograms";
+            this.cdPrograms.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.cdPrograms.Size = new System.Drawing.Size(110, 26);
+            this.cdPrograms.TabIndex = 2;
+            this.cdPrograms.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.cdPrograms.Title = "Programs";
+            this.cdPrograms.UnderlineColor = System.Drawing.Color.Black;
+            this.cdPrograms.UnderlineHight = 3;
+            // 
+            // cdHome
+            // 
+            this.cdHome.HighlightColor = System.Drawing.SystemColors.Highlight;
+            this.cdHome.IsSelected = false;
+            this.cdHome.Location = new System.Drawing.Point(12, 32);
+            this.cdHome.Name = "cdHome";
+            this.cdHome.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.cdHome.Size = new System.Drawing.Size(110, 26);
+            this.cdHome.TabIndex = 1;
+            this.cdHome.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.cdHome.Title = "Home";
+            this.cdHome.UnderlineColor = System.Drawing.Color.Black;
+            this.cdHome.UnderlineHight = 3;
+            // 
+            // iPage
+            // 
+            this.iPage.Count = 1;
+            this.iPage.HoverColor = System.Drawing.SystemColors.Highlight;
+            this.iPage.IndicatorColor = System.Drawing.Color.Black;
+            this.iPage.IndicatorCursor = System.Windows.Forms.Cursors.Hand;
+            this.iPage.IndicatorSize = new System.Drawing.Size(8, 8);
+            this.iPage.Location = new System.Drawing.Point(317, 455);
+            this.iPage.Name = "iPage";
+            this.iPage.Selected = 1;
+            this.iPage.SelectionColor = System.Drawing.SystemColors.Highlight;
+            this.iPage.Size = new System.Drawing.Size(8, 8);
+            this.iPage.Spacing = 5;
+            this.iPage.TabIndex = 0;
+            // 
+            // cdPage
+            // 
+            this.cdPage.HighlightColor = System.Drawing.Color.Black;
+            this.cdPage.IsSelected = false;
+            this.cdPage.Location = new System.Drawing.Point(306, 422);
+            this.cdPage.Name = "cdPage";
+            this.cdPage.SelectionColor = System.Drawing.Color.Black;
+            this.cdPage.Size = new System.Drawing.Size(30, 22);
+            this.cdPage.TabIndex = 7;
+            this.cdPage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.cdPage.Title = "1";
+            this.cdPage.UnderlineColor = System.Drawing.Color.Black;
+            this.cdPage.UnderlineHight = 2;
+            // 
+            // searchBox
+            // 
+            this.searchBox.BackColor = System.Drawing.SystemColors.Window;
+            this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchBox.Location = new System.Drawing.Point(337, 32);
+            this.searchBox.MaximumSize = new System.Drawing.Size(261, 31);
+            this.searchBox.MinimumSize = new System.Drawing.Size(261, 31);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(261, 31);
+            this.searchBox.TabIndex = 6;
+            // 
             // edStudents
             // 
             this.edStudents.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.edStudents.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.edStudents.Location = new System.Drawing.Point(63, 115);
             this.edStudents.Name = "edStudents";
+            this.edStudents.Page = 1;
             this.edStudents.PerPage = 5;
             this.edStudents.Selected = null;
             this.edStudents.SelectionColor = System.Drawing.SystemColors.Highlight;
@@ -135,76 +239,11 @@
             this.edStudents.Spacing = 5;
             this.edStudents.TabIndex = 0;
             // 
-            // cdProfessors
-            // 
-            this.cdProfessors.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cdProfessors.HighlightColor = System.Drawing.SystemColors.Highlight;
-            this.cdProfessors.IsSelected = false;
-            this.cdProfessors.Location = new System.Drawing.Point(12, 274);
-            this.cdProfessors.Name = "cdProfessors";
-            this.cdProfessors.SelectionColor = System.Drawing.SystemColors.Highlight;
-            this.cdProfessors.Size = new System.Drawing.Size(110, 29);
-            this.cdProfessors.TabIndex = 5;
-            this.cdProfessors.Title = "Professors";
-            this.cdProfessors.UnderlineColor = System.Drawing.Color.Black;
-            // 
-            // cdStudents
-            // 
-            this.cdStudents.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cdStudents.HighlightColor = System.Drawing.SystemColors.Highlight;
-            this.cdStudents.IsSelected = false;
-            this.cdStudents.Location = new System.Drawing.Point(12, 215);
-            this.cdStudents.Name = "cdStudents";
-            this.cdStudents.SelectionColor = System.Drawing.SystemColors.Highlight;
-            this.cdStudents.Size = new System.Drawing.Size(110, 29);
-            this.cdStudents.TabIndex = 4;
-            this.cdStudents.Title = "Students";
-            this.cdStudents.UnderlineColor = System.Drawing.Color.Black;
-            // 
-            // cdCourses
-            // 
-            this.cdCourses.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cdCourses.HighlightColor = System.Drawing.SystemColors.Highlight;
-            this.cdCourses.IsSelected = false;
-            this.cdCourses.Location = new System.Drawing.Point(12, 152);
-            this.cdCourses.Name = "cdCourses";
-            this.cdCourses.SelectionColor = System.Drawing.SystemColors.Highlight;
-            this.cdCourses.Size = new System.Drawing.Size(110, 29);
-            this.cdCourses.TabIndex = 3;
-            this.cdCourses.Title = "Courses";
-            this.cdCourses.UnderlineColor = System.Drawing.Color.Black;
-            // 
-            // cdPrograms
-            // 
-            this.cdPrograms.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cdPrograms.HighlightColor = System.Drawing.SystemColors.Highlight;
-            this.cdPrograms.IsSelected = false;
-            this.cdPrograms.Location = new System.Drawing.Point(12, 91);
-            this.cdPrograms.Name = "cdPrograms";
-            this.cdPrograms.SelectionColor = System.Drawing.SystemColors.Highlight;
-            this.cdPrograms.Size = new System.Drawing.Size(110, 29);
-            this.cdPrograms.TabIndex = 2;
-            this.cdPrograms.Title = "Programs";
-            this.cdPrograms.UnderlineColor = System.Drawing.Color.Black;
-            // 
-            // cdHome
-            // 
-            this.cdHome.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cdHome.HighlightColor = System.Drawing.SystemColors.Highlight;
-            this.cdHome.IsSelected = false;
-            this.cdHome.Location = new System.Drawing.Point(12, 32);
-            this.cdHome.Name = "cdHome";
-            this.cdHome.SelectionColor = System.Drawing.SystemColors.Highlight;
-            this.cdHome.Size = new System.Drawing.Size(110, 29);
-            this.cdHome.TabIndex = 1;
-            this.cdHome.Title = "Home";
-            this.cdHome.UnderlineColor = System.Drawing.Color.Black;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 461);
+            this.ClientSize = new System.Drawing.Size(791, 481);
             this.Controls.Add(this.cdProfessors);
             this.Controls.Add(this.cdStudents);
             this.Controls.Add(this.cdCourses);
@@ -237,6 +276,8 @@
         private ui.display.EntityDisplayGroup edStudents;
         private System.Windows.Forms.PictureBox picLink;
         private ui.functionality.SearchBox searchBox;
+        private ui.display.ClickableDisplay cdPage;
+        private ui.display.Indicator iPage;
     }
 }
 
--- student-manager/Form1.cs
+++ student-manager/Form1.cs
@@ -16,28 +16,51 @@ namespace student_manager
         public Form1()
         {
             InitializeComponent();
+
             edStudents.SelectionChanged += (sender, args) =>
             {
-
-                bool isVisible = edStudents.Selected != null;
+                var isVisible = edStudents.Selected != null;
 
                 picEdit.Visible = isVisible;
                 picMinus.Visible = isVisible;
                 picLink.Visible = isVisible;
+
+                Console.WriteLine($"[{DateTime.Now}] Current Selection \"{edStudents.Selected}\"");
             };
 
-            searchBox.Searched += (sender, e) =>
+            searchBox.Searched += (sender, e) => 
             {
 
-                Console.WriteLine($"New User Search: \"{searchBox.Text}\"");
+                Console.WriteLine($"[{DateTime.Now}] New User Search: \"{searchBox.Text}\"");
             };
 
-            edStudents.AddEntity(new Professor("A115H", new info.Name("Max", "Lee"), DateTime.Now, Gender.Male, DateTime.Now, true));
-            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male, DateTime.Now));
-            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male, DateTime.Now));
-            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male, DateTime.Now));
-            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male, DateTime.Now));
-            edStudents.AddEntity(new Student("A2215H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male, DateTime.Now));
+            edStudents.MaxChanged += (sender, args) =>
+            {
+
+                Console.WriteLine($"[{DateTime.Now}] The max page has changed to {edStudents.MaxPages}");
+                iPage.Count = edStudents.MaxPages;
+            };
+
+            iPage.SelectionChanged += (sender, args) =>
+            {
+
+                Console.WriteLine($"[{DateTime.Now}] Page change requested to page {iPage.Selected}");
+                edStudents.Page = iPage.Selected;
+                cdPage.Title = iPage.Selected.ToString();
+            };
+
+            edStudents.AddEntity(new Professor("A115H", new info.Name("Max", "Lee"), DateTime.Now, Gender.Male,
+                DateTime.Now, true));
+            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male,
+                DateTime.Now));
+            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male,
+                DateTime.Now));
+            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male,
+                DateTime.Now));
+            edStudents.AddEntity(new Student("A115H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male,
+                DateTime.Now));
+            edStudents.AddEntity(new Student("A2215H", new info.Name("Art", "Punk"), DateTime.Now, Gender.Male,
+                DateTime.Now));
         }
     }
 }
