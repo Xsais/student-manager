689436a481308bd24d6a879086f34dd002c80a96
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-08-11 00:06:14 -0400

    Handles visual displaying errors in input

---
+++ student-manager/ui/functionality/ErrorTextBox.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using student_manager.info;
+
+namespace student_manager.ui.functionality
+{
+    public partial class ErrorTextBox : UserControl
+    {
+        private bool _shownPlaceHolder;
+
+        public override string Text
+        {
+            get => string.Equals(txtText.Text, PlaceHolder) ? "" : txtText.Text;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+
+                    ShowPlaceHolder(this, EventArgs.Empty);
+                    return;
+                }
+
+                HidePlaceHolder(this, EventArgs.Empty);
+                txtText.Text = value;
+            }
+        }
+
+        public Color ErrorColor
+        {
+            get => _errorColor;
+            set
+            {
+                if (Status == Status.Error)
+                {
+                    lblNotifications.BackColor = value;
+                }
+
+                _errorColor = value;
+            }
+        }
+
+        public Color StatusColor
+        {
+            get => _statusColor;
+            set
+            {
+                if (Status == Status.Normal)
+                {
+                    lblNotifications.BackColor = value;
+                }
+
+                _statusColor = value;
+            }
+        }
+
+        public String PlaceHolder
+        {
+            get => _placeHolder;
+            set
+            {
+                if (_shownPlaceHolder)
+                {
+                    txtText.Text = value;
+                }
+
+                _placeHolder = value;
+            }
+        }
+
+        public Color PlaceColor
+        {
+            get => _placeColor;
+            set
+            {
+                if (_shownPlaceHolder)
+                {
+                    txtText.ForeColor = value;
+                }
+
+                _placeColor = value;
+            }
+        }
+
+        public Status Status
+        {
+            get => _status;
+            set
+            {
+                switch (value)
+                {
+                    case Status.Error:
+
+                        lblNotifications.BackColor = ErrorColor;
+                        break;
+                    case Status.Normal:
+
+                        lblNotifications.BackColor = StatusColor;
+                        break;
+                }
+
+                _status = value;
+            }
+        }
+
+        public new EventHandler TextChanged;
+        private Status _status;
+        private string _placeHolder;
+        private Color _placeColor = SystemColors.GrayText;
+        private Color _errorColor = Color.FromArgb(183, 28, 28);
+        private Color _statusColor = Color.Black;
+
+        public ErrorTextBox()
+        {
+            InitializeComponent();
+
+            Status = Status.Normal;
+        }
+
+        private void InProgress(object sender, EventArgs e)
+        {
+            _shownPlaceHolder = string.IsNullOrWhiteSpace(txtText.Text);
+            TextChanged?.Invoke(sender, e);
+        }
+
+        private void HidePlaceHolder(object sender, EventArgs e)
+        {
+            if (!_shownPlaceHolder)
+            {
+                return;
+            }
+
+            txtText.Text = "";
+            txtText.ForeColor = ForeColor;
+        }
+
+        private void ShowPlaceHolder(object sender, EventArgs e)
+        {
+            if (!_shownPlaceHolder)
+            {
+                return;
+            }
+
+            txtText.Text = PlaceHolder;
+            txtText.ForeColor = PlaceColor;
+        }
+    }
+}
---
+++ student-manager/ui/functionality/ErrorTextBox.designer.cs
@@ -0,0 +1,76 @@
+﻿namespace student_manager.ui.functionality
+{
+    partial class ErrorTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNotifications = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNotifications
+            // 
+            this.lblNotifications.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblNotifications.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblNotifications.Location = new System.Drawing.Point(0, 20);
+            this.lblNotifications.Name = "lblNotifications";
+            this.lblNotifications.Size = new System.Drawing.Size(128, 2);
+            this.lblNotifications.TabIndex = 0;
+            // 
+            // txtText
+            // 
+            this.txtText.BackColor = System.Drawing.SystemColors.Control;
+            this.txtText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtText.Location = new System.Drawing.Point(0, 0);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(128, 15);
+            this.txtText.TabIndex = 1;
+            this.txtText.TextChanged += new System.EventHandler(this.InProgress);
+            this.txtText.Enter += new System.EventHandler(this.HidePlaceHolder);
+            this.txtText.Leave += new System.EventHandler(this.ShowPlaceHolder);
+            // 
+            // ErrorTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblNotifications);
+            this.Controls.Add(this.txtText);
+            this.Name = "ErrorTextBox";
+            this.Size = new System.Drawing.Size(128, 22);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNotifications;
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
