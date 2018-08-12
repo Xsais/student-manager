0963a48bade038abe564010f9fbd029b64d6f5a1
Nathaniel Primo <nathaniel.primo@gmail.com>
2018-08-12 19:19:20 -0400

    Initial support for links

--- student-manager/info/Entity.cs
+++ student-manager/info/Entity.cs
@@ -8,6 +8,36 @@ namespace student_manager.info.entity
 {
     public abstract class Entity
     {
+        private readonly List<Entity> _links = new List<Entity>();
+
+        public Entity this[int index] => _links[index];
+
+        public void AddLink(Entity link)
+        {
+            if (link == null && _links.FirstOrDefault(entity => entity.Equals(link)) != null)
+            {
+                return;
+            }
+
+            _links.Add(link);
+        }
+
+        public void RemoveLink(int index) => _links.RemoveAt(index);
+
+        public void RemoveLink(string id)
+        {
+
+            var selected = _links.FirstOrDefault(entity => entity.ID.Equals(id));
+
+            if (selected == null)
+            {
+
+                return;
+            }
+
+            RemoveLink(_links.IndexOf(selected));
+        }
+
         public string ID { get; set; }
 
         public Entity(string id)
@@ -28,7 +58,7 @@ namespace student_manager.info.entity
                 return false;
             }
 
-            return ((Entity)obj).ID == ID;
+            return string.Equals(((Entity)obj).ID, ID);
         }
 
         public bool IsEmpty() => ID == null || ID.Equals("");
